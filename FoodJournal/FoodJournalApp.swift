fd5cf91b080a2d5e3a68b60d9547e777fc10199b
Mike Shaw <shawbler@gmail.com>
2026-04-30 15:14:48 -0700

    Initial Commit

---
+++ FoodJournal/FoodJournalApp.swift
@@ -0,0 +1,32 @@
+//
+//  FoodJournalApp.swift
+//  FoodJournal
+//
+//  Created by Mike Shaw on 4/30/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FoodJournalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
